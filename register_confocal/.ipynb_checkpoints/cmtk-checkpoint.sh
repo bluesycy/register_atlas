d96c3b8c07c745d0fcdb9ba89d629cd90f0271a2
Chuyu Yang <bluesycy@gmail.com>
2025-03-13 20:53:12 +0100

    test confocal

---
+++ register_confocal/.ipynb_checkpoints/cmtk-checkpoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+DATASET_NAME=$1
+SERVER=$2
+ANALYZER=$3
+# REF_PATH="/nfs/data${SERVER}/${ANALYZER}/data/${DATASET_NAME}/${DATASET_NAME}_confocal_rotated.nrrd"
+REF_PATH="/nfs/data${SERVER}/${ANALYZER}/data/${DATASET_NAME}/${DATASET_NAME}_confocal_rotated_flipped.nrrd"
+DATA_PATH="/nfs/data${SERVER}/${ANALYZER}/data/${DATASET_NAME}"
+
+IMG_PATH="${DATA_PATH}/${DATASET_NAME}_ds_stack_mean.nrrd"
+RESULT_PATH="${DATA_PATH}/${DATASET_NAME}_confocal_cmtk"
+OUTPUT_NAME="${DATASET_NAME}_registered.nrrd"
+
+
+OUTPUT_NAME_INIT="${DATASET_NAME}_registered_initial.nrrd"
+OUTPUT_NAME_RIGID="${DATASET_NAME}_registered_rigid.nrrd"
+OUTPUT_NAME_WARP="${DATASET_NAME}_registered_warp.nrrd"
+
+OUTPUT_NAME_INIT_INV="${DATASET_NAME}_registered_initial_inv.nrrd"
+OUTPUT_NAME_RIGID_INV="${DATASET_NAME}_registered_rigid_inv.nrrd"
+OUTPUT_NAME_WARP_INV="${DATASET_NAME}_registered_warp_inv.nrrd"
+
+echo "make_initial_affine"
+cmtk make_initial_affine --centers-of-mass $IMG_PATH $REF_PATH $RESULT_PATH/initial.list
+
+echo "registration"
+cmtk registration --initial $RESULT_PATH/initial.list --nmi --threads 64 --dofs 6 --dofs 12 --nmi --exploration 8 --accuracy 0.8 -o $RESULT_PATH/affine.list $IMG_PATH $REF_PATH
+
+echo "warp"
+cmtk warp --nmi --threads 64 --jacobian-weight 0 --fast -e 18 --grid-spacing 100 --energy-weight 1e-1 --refine 4 --coarsest 10 --ic-weight 0 --output-intermediate --accuracy 0.5 -o $RESULT_PATH/warp.list $RESULT_PATH/affine.list
+
+echo "reformatx"
+cmtk reformatx --pad-out 0 -o $RESULT_PATH/$OUTPUT_NAME_INIT --floating $REF_PATH $IMG_PATH $RESULT_PATH/initial.list
+cmtk reformatx --pad-out 0 -o $RESULT_PATH/$OUTPUT_NAME_RIGID --floating $REF_PATH $IMG_PATH $RESULT_PATH/affine.list
+cmtk reformatx --pad-out 0 -o $RESULT_PATH/$OUTPUT_NAME_WARP --floating $REF_PATH $IMG_PATH $RESULT_PATH/warp.list
+
+cmtk reformatx --pad-out 0 -o $RESULT_PATH/$OUTPUT_NAME_INIT_INV --floating $IMG_PATH $REF_PATH --inverse $RESULT_PATH/initial.list
+cmtk reformatx --pad-out 0 -o $RESULT_PATH/$OUTPUT_NAME_RIGID_INV --floating $IMG_PATH $REF_PATH --inverse $RESULT_PATH/affine.list
+cmtk reformatx --pad-out 0 -o $RESULT_PATH/$OUTPUT_NAME_WARP_INV --floating $IMG_PATH $REF_PATH --inverse $RESULT_PATH/warp.list
+
+
+echo "done!"
